5bb06ba73f32ed9b71dc2381695a49a68840a2e3
Dong-ryull Shin / Ryan <1982sdr@hanmail.net>
2026-09-19 14:57:15 +0900

    ipowiki 20260919_02 - modify .vitepress/config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
         items: [
           { text: '증권사', link: '/broker' },
           { text: '확정공고', link: '/stock' },
+          { text: '환매청구권', link: '/putback' },
         ]
       }
     ],
